d23815ba4c343b1640119af097effab597843f3b
ei-sei <74192522+ei-sei@users.noreply.github.com>
2023-04-12 14:19:53 +0100

    change

--- src/components/SearchWidget/index.jsx
+++ src/components/SearchWidget/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { SearchForm, ShowList, FilterSelection } from '../';
 
 export default function SearchWidget() {
@@ -6,41 +6,39 @@ export default function SearchWidget() {
     const [shows, setShows] = useState([]);
     const [sortOrder, setSortOrder] = useState("asc");
 
+    // API call to fetch TV show data based on search query
     async function searchAPI(searchString) {
         const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchString}`);
         const rawData = await response.json();
         const data = rawData.map(s => s.show);
         setShows(data)
     }
-
+    // useEffect hook to call searchAPI function when the search query changes
     useEffect(() => {
         searchAPI(searchString);
     }, [searchString])
 
+    // Handler function for updating the sort order state variable based on user selection
     function handleSortOrder(selectedValue) {
         setSortOrder(selectedValue);
     }
-
-    function sortShowsByRating() {
-        const sortedShows = shows.sort((a, b) => {
+    // Function to sort the list of TV shows based on rating and sort order
+    const sortedShows = useMemo(() => {
+        const sorted = [...shows].sort((ascend, descend) => {
             if (sortOrder === "asc") {
-                return a.rating.average - b.rating.average;
+                return ascend.rating.average - descend.rating.average;
             } else {
-                return b.rating.average - a.rating.average;
+                return descend.rating.average - ascend.rating.average;
             }
         });
-        setShows([...sortedShows]);
-    }
-
-    useEffect(() => {
-        sortShowsByRating();
-    }, [sortOrder])
+        return sorted;
+    }, [shows, sortOrder]);
 
     return (
         <>
             <SearchForm onSubmit={setSearchString} />
             <FilterSelection onSort={handleSortOrder} />
-            <ShowList shows={shows} />
+            <ShowList shows={sortedShows} />
         </>
-    )
+    );
 }
